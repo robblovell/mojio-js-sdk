ce8d3dca938ae745748e338e963e8c8988fe6854
Robb Lovell <robbl@moj.io>
2014-04-23 21:45:10 -0700

    removed build, just test now

--- test/login_test.js
+++ test/login_test.js
@@ -40,6 +40,26 @@
     });
   });
 
+  config = {
+    application: 'YOUR APPLICATION KEY',
+    secret: 'YOUR SECRET KEY',
+    hostname: 'staging.api.moj.io',
+    version: 'v1',
+    port: '80'
+  };
+
+  Mojio = require('../dist/nodejs/Mojio.js');
+
+  mojio = new Mojio(config);
+
+  mojio.login('YOUR USERNAME', 'YOUR PASSWORD', function(error, result) {
+    if (error) {
+      return Console.log("error: " + error);
+    } else {
+      return console.log("success:" + result);
+    }
+  });
+
 }).call(this);
 
 /*
