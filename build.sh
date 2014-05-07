904e36d5e2405b41625031eab14b78b03a9ac92e
Robb Lovell <robblovell@gmail.com>
2014-05-06 22:31:51 -0700

    travis.yml fix to test runner

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ bower install
 coffee --map --compile src/nodejs
 cp src/nodejs/*.js lib/
 coffee --map --compile src/browser
+mocha test
 cd src/browser
 ../../node_modules/.bin/browserify -r ./HttpBrowserWrapper.js --standalone HttpBrowserWrapper > ../../dist/browser/HttpBrowserWrapper.js
 ../../node_modules/.bin/browserify -r ./MojioClient.js --standalone MojioClient > ../../dist/browser/MojioClient.js
 cd ../../
-mocha
