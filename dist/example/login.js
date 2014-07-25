4e08c3d0b18cd1dd9ed3339300c7e94a12e26c4d
Robb Lovell <robbl@moj.io>
2014-07-24 17:37:17 -0700

    Fixed example keys.

--- dist/example/login.js
+++ dist/example/login.js
@@ -6,7 +6,7 @@
 
     config = {
         application: 'f201b929-d28c-415d-9b71-8112532301cb',
-        secret: 'ee6fbd50-8c34-4cc1-bd78-d36891f43d10',
+        secret: '2ef80a7a-780d-41c1-8a02-13a286f11a23',
         hostname: 'staging.api.moj.io',
         version: 'v1',
         port: '80',
